26f0519a35dd71aa3066fcdfaa900ad39abadb4a
mpmkp2020 <madhukarhbti@gmail.com>
2020-09-14 13:53:16 +0530

    Update install_yq.sh

--- .travis/install_yq.sh
+++ .travis/install_yq.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-curl -L https://github.com/mikefarah/yq/releases/download/3.3.2/yq_linux_amd64 > yq && chmod +x yq
+if [ `uname -m` = 'aarch64' ]; then
+   curl -L https://github.com/mikefarah/yq/releases/download/3.3.2/yq_linux_arm64 > yq && chmod +x yq
+else
+   curl -L https://github.com/mikefarah/yq/releases/download/3.3.2/yq_linux_amd64 > yq && chmod +x yq
+fi
+
 sudo cp yq /usr/bin/
