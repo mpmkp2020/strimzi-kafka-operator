35c6be1c89e040a5ade21ab54fe69594649157dd
mpmkp2020 <madhukarhbti@gmail.com>
2020-09-16 11:01:25 +0530

    Update setup-kubernetes.sh

--- .travis/setup-kubernetes.sh
+++ .travis/setup-kubernetes.sh
@@ -86,7 +86,8 @@ if [ "$TEST_CLUSTER" = "minikube" ]; then
 
     docker run -d -p 5000:5000 registry
     #sudo apt-get -y install --reinstall linux-image-`uname -r`
-    sudo apt install -y kubelet kubeadm kubectl kubernetes-cni
+    #sudo apt install -y kubelet kubeadm kubectl kubernetes-cni
+    find / -name "*config*"
     export KUBECONFIG=$HOME/.kube/config
     sudo -E minikube start --vm-driver=none --kubernetes-version=v1.15.0 \
       --insecure-registry=localhost:5000 --extra-config=apiserver.authorization-mode=RBAC
