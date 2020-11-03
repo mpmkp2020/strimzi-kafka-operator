a3cf21fa4e29755e4743e5e8ddc573e24fcf5a8b
mpmkp2020 <madhukarhbti@gmail.com>
2020-11-03 18:17:40 +0530

    Update setup-kubernetes.sh

--- .travis/setup-kubernetes.sh
+++ .travis/setup-kubernetes.sh
@@ -1,137 +1,40 @@
-#!/usr/bin/env bash
-set -x
+#!/bin/bash
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-rm -rf ~/.kube
+set -ex
 
-function install_kubectl {
-    if [ "${TEST_KUBECTL_VERSION:-latest}" = "latest" ]; then
-        TEST_KUBECTL_VERSION=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
-    fi
-    if [ `uname -m` = 'aarch64' ]; then
-        curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/${TEST_KUBECTL_VERSION}/bin/linux/arm64/kubectl && chmod +x kubectl
-    else
-        curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/${TEST_KUBECTL_VERSION}/bin/linux/amd64/kubectl && chmod +x kubectl
-    fi
-    sudo cp kubectl /usr/bin
-}
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/arm64/kubectl
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+kubectl version --client
 
-function install_nsenter {
-    # Pre-req for helm
-    curl https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v${TEST_NSENTER_VERSION}/util-linux-${TEST_NSENTER_VERSION}.tar.gz -k | tar -zxf-
-    cd util-linux-${TEST_NSENTER_VERSION}
-    ./configure --without-ncurses
-    make nsenter
-    sudo cp nsenter /usr/bin
-}
-
-function wait_for_minikube {
-    i="0"
-
-    while [ $i -lt 60 ]
-    do
-        # The role needs to be added because Minikube is not fully prepared for RBAC.
-        # Without adding the cluster-admin rights to the default service account in kube-system
-        # some components would be crashing (such as KubeDNS). This should have no impact on
-        # RBAC for Strimzi during the system tests.
-        kubectl create clusterrolebinding add-on-cluster-admin --clusterrole=cluster-admin --serviceaccount=kube-system:default
-        if [ $? -ne 0 ]
-        then
-            sleep 1
-        else
-            return 0
-        fi
-        i=$[$i+1]
-    done
-
-    return 1
-}
-
-function label_node {
-
-    if [ "$TEST_CLUSTER" = "minikube" ]; then
-        echo $(kubectl get nodes)
-        kubectl label node minikube rack-key=zone
-    elif [ "$TEST_CLUSTER" = "minishift" ]; then
-        oc label node minishift rack-key=zone
-    elif [ "$TEST_CLUSTER" = "oc" ]; then
-        oc label node localhost rack-key=zone
-    fi
-}
-
-if [ "$TEST_CLUSTER" = "minikube" ]; then
-    install_kubectl
-   if [ `uname -m` = 'aarch64' ]; then
-        if [ "${TEST_MINIKUBE_VERSION:-latest}" = "latest" ]; then
-            TEST_MINIKUBE_URL=https://storage.googleapis.com/minikube/releases/latest/minikube-linux-arm64
-        else
-            TEST_MINIKUBE_URL=https://github.com/kubernetes/minikube/releases/download/${TEST_MINIKUBE_VERSION}/minikube-linux-arm64
-        fi
-    else
-        if [ "${TEST_MINIKUBE_VERSION:-latest}" = "latest" ]; then
-            TEST_MINIKUBE_URL=https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-        else
-            TEST_MINIKUBE_URL=https://github.com/kubernetes/minikube/releases/download/${TEST_MINIKUBE_VERSION}/minikube-linux-amd64
-        fi
-    fi
-    curl -Lo minikube ${TEST_MINIKUBE_URL} && chmod +x minikube
-    sudo cp minikube /usr/bin
-
-    export MINIKUBE_WANTUPDATENOTIFICATION=false
-    export MINIKUBE_WANTREPORTERRORPROMPT=false
-    export MINIKUBE_HOME=$HOME
-    export CHANGE_MINIKUBE_NONE_USER=true
-    
-    mkdir $HOME/.kube || true
-    touch $HOME/.kube/config
-
-    docker run -d -p 5000:5000 registry
-    sudo echo "deb http://ports.ubuntu.com/ubuntu-ports/ bionic-updates main" >> /etc/apt/sources.list
-    sudo apt update
-    sudo apt-get -y install linux-image-`uname -r`
-    sudo apt-get -qq -y install conntrack
-    export KUBECONFIG=$HOME/.kube/config
-    #sudo -E minikube start --vm-driver=none --kubernetes-version=v1.15.0 \
-    #  --insecure-registry=localhost:5000 --extra-config=apiserver.authorization-mode=RBAC
-    sudo -E minikube start --vm-driver=none
-    sudo minikube status
-    sudo chown -R travis: /home/travis/.minikube/
-    sudo -E minikube addons enable default-storageclass
-
-    wait_for_minikube
-
-    if [ $? -ne 0 ]
-    then
-        echo "Minikube failed to start or RBAC could not be properly set up"
-        exit 1
-    fi
-
-elif [ "$TEST_CLUSTER" = "minishift" ]; then
-    #install_kubectl
-    MS_VERSION=1.13.1
-    curl -Lo minishift.tgz https://github.com/minishift/minishift/releases/download/v$MS_VERSION/minishift-$MS_VERSION-linux-amd64.tgz && tar -xvf minishift.tgz --strip-components=1 minishift-$MS_VERSION-linux-amd64/minishift && rm minishift.tgz && chmod +x minishift
-    sudo cp minishift /usr/bin
-
-    #export MINIKUBE_WANTUPDATENOTIFICATION=false
-    #export MINIKUBE_WANTREPORTERRORPROMPT=false
-    export MINISHIFT_HOME=$HOME
-    #export CHANGE_MINIKUBE_NONE_USER=true
-    mkdir $HOME/.kube || true
-    touch $HOME/.kube/config
-    docker run -d -p 5000:5000 registry
-
-    export KUBECONFIG=$HOME/.kube/config
-    sudo -E minishift start
-    sudo -E minishift addons enable default-storageclass
-elif [ "$TEST_CLUSTER" = "oc" ]; then
-    mkdir -p /tmp/openshift
-    wget https://github.com/openshift/origin/releases/download/v3.7.0/openshift-origin-client-tools-v3.7.0-7ed6862-linux-64bit.tar.gz -O openshift.tar.gz
-    tar xzf openshift.tar.gz -C /tmp/openshift --strip-components 1
-    sudo cp /tmp/openshift/oc /usr/bin
-else
-    echo "Unsupported TEST_CLUSTER '$TEST_CLUSTER'"
-    exit 1
+# conntrack is required for minikube 1.19 and higher for none driver
+if ! conntrack --version &>/dev/null; then
+  echo "WARNING: No contrack is not installed"
+  sudo apt-get update -qq
+  sudo apt-get -qq -y install conntrack
 fi
 
-if [ "$TRAVIS" = false ]; then
-    label_node
-fi
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-arm64
+chmod +x minikube
+sudo mv minikube /usr/local/bin/
+
+sudo apt-get update
+sudo apt-get install -y liblz4-tool
+
+sudo minikube start --vm-driver=none
+sudo minikube status
+sudo chown -R $USER $HOME/.kube $HOME/.minikube
+kubectl cluster-info
